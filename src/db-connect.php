8e1d11eab181167d467f06d6b384a789b7d56915
Kaito Hirayama <2201022@s.asojuku.ac.jp>
2024-01-12 13:34:50 +0900

    change password

--- src/db-connect.php
+++ src/db-connect.php
@@ -2,7 +2,7 @@
     const SERVER = 'mysql220.phy.lolipop.lan';
     const DBNAME = 'LAA1516914-final';
     const USER = 'LAA1516914';
-    const PASS = 'PASS0714';
+    const PASS = 'Pass0714';
 
     $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
 ?>
